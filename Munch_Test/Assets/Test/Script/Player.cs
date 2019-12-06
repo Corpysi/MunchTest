7dfdfb416c8cbb2e77dc25bcd05fd87267a8e5f4
m0118069 <56628609+m0118069@users.noreply.github.com>
2019-12-06 23:24:56 +0900

    移動の追加（片桐蓮）

---
+++ Munch_Test/Assets/Test/Script/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //移動
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += new Vector3(-Speed, 0,0);
+        }
+
+       else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += new Vector3(Speed, 0, 0);
+        }
+    }
+}
